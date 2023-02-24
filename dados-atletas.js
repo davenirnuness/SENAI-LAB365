09de3b08e946f61211afd8475a5e869be96bd684
Davenir Pedro Nunes <117782102+davenirnuness@users.noreply.github.com>
2023-02-23 22:45:01 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,55 @@
+let atletas = [
+{
+    nome: "Cesar Abascal",
+    notas: [10,9.34,8.42,10,7.88]
+},
+{
+    nome: "Fernando Puntel",
+    notas: [8,10,10,7,9.33]
+},
+{
+    nome: "Daiane Jelinsky",
+    notas: [7,10,9.5,9.5,8]
+},
+{
+    nome:"Bruno Castro",
+    notas: [10,10,10,9,9.5]
+}
+];
+   let nomeDosCompetidores = 0
+   let mediaValida = 0
+   let notasObtidas = 0
+   for (let i = 0; i, < atletas.length; i++){
+
+       nomeDosCompetidores = atletas.map(function(nomes){
+           return nomes.nome
+       })
+       notasObtidas = atletas.map(function(notas){
+           let total =  notas.nota
+           total.sort(function(a,b){
+               return a - b;
+           });
+           return total
+       })
+
+   }
+
+   for (let i = 0; i, < atletas.length;i++){
+     switch(i){
+         case i:
+         console.log(atletas: ${nomeDosCompetidores[i]})
+         consol.log(Notas Obtidas: ${notasObtidas[i]})
+    let notasAtleta = notasObtidas[i].slice(1,notasObtidas[i].length-1) 
+    let soma =
+    notasAtleta.reduce(function(total,atual){
+        return (total + atual)
+    })
+    let media = soma/notasAtleta.length
+    console.log(media valida ${media})
+      console.log(
+          break
+          default:
+          console.log("Atleta não cadastrado");
+
+     }
+   }
